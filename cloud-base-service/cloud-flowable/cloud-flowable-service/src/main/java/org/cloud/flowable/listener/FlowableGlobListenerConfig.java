393db7529961e148d1b147b451e63f38ffa5b234
liulei <x157123123@gmail.com>
2023-11-29 23:33:17 +0800

    代码提交

---
+++ cloud-base-service/cloud-flowable/cloud-flowable-service/src/main/java/org/cloud/flowable/listener/FlowableGlobListenerConfig.java
@@ -0,0 +1,42 @@
+package org.cloud.flowable.listener;
+
+import org.flowable.common.engine.api.delegate.event.FlowableEngineEventType;
+import org.flowable.common.engine.api.delegate.event.FlowableEventDispatcher;
+import org.flowable.spring.SpringProcessEngineConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ContextRefreshedEvent;
+
+/**
+ * Flowable全局监听配置
+ * 用途:在任务特殊节点或者流程的特殊节点做一些自定义操作
+ *
+ * @author gblfy
+ * @Date 2022-06-21
+ **/
+@Configuration
+public class FlowableGlobListenerConfig implements ApplicationListener<ContextRefreshedEvent> {
+
+    @Autowired
+    private SpringProcessEngineConfiguration configuration;
+
+    @Autowired
+    private GlobalProcessStartedListener globalProcessStartedListener;
+
+    @Autowired
+    private GlobalProcessEndListener globalProcessEndListener;
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        
+        FlowableEventDispatcher dispatcher = configuration.getEventDispatcher();
+
+        //流程开始全局监听
+        dispatcher.addEventListener(globalProcessStartedListener, FlowableEngineEventType.PROCESS_STARTED);
+
+        //流程结束全局监听
+        dispatcher.addEventListener(globalProcessEndListener, FlowableEngineEventType.PROCESS_COMPLETED);
+    }
+
+}
---
+++ cloud-base-service/cloud-flowable/cloud-flowable-service/src/main/java/org/cloud/flowable/listener/GlobalProcessEndListener.java
@@ -0,0 +1,48 @@
+package org.cloud.flowable.listener;
+
+import org.flowable.common.engine.api.delegate.event.FlowableEngineEntityEvent;
+import org.flowable.engine.delegate.event.AbstractFlowableEngineEventListener;
+import org.flowable.engine.delegate.event.impl.FlowableEntityEventImpl;
+import org.flowable.engine.impl.persistence.entity.ExecutionEntityImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * 流程结束全局监听器
+ *
+ * @author: gblfy
+ * @date 2022-06-21
+ */
+@Component
+public class GlobalProcessEndListener extends AbstractFlowableEngineEventListener {
+    protected Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    protected void processCompleted(FlowableEngineEntityEvent event) {
+        logger.info("进入流程完成监听器------------------------Start---------------------->");
+        String eventName = event.getType().name();
+
+        FlowableEntityEventImpl flowableEntityEvent = (FlowableEntityEventImpl) event;
+        ExecutionEntityImpl processInstance = (ExecutionEntityImpl) flowableEntityEvent.getEntity();
+
+        Date startTime = processInstance.getStartTime();
+        String processDefinitionKey = processInstance.getProcessDefinitionKey();
+        String processInstanceId = processInstance.getProcessInstanceId();
+        String processInstanceBusinessKey = processInstance.getProcessInstanceBusinessKey();
+        int suspensionState = processInstance.getSuspensionState();
+
+
+        logger.info("流程事件类型->{}", eventName);
+        logger.info("流程开始时间->{}", startTime);
+        logger.info("流程定义Key->{}", processDefinitionKey);
+        logger.info("流程实例ID->{}", processInstanceId);
+        logger.info("流程业务key->{}", processInstanceBusinessKey);
+        logger.info("流程是否挂起标志->{}", suspensionState);
+
+        logger.info("流程完成监听器------------------------End---------------------->");
+    }
+
+}
